aa976a097002c6e5ccb69f4f84ed0528d4913a61
Martica <58595345+Martica199005@users.noreply.github.com>
2020-07-12 10:31:41 +0200

    Add files via upload

---
+++ Responsive-web-page/script.js
@@ -0,0 +1,11 @@
+function newsletter(){
+    var email= prompt('Insert e-mail');
+    var h1= document.createElement('h1');
+    var h1= document.createElement('h1');
+    var textAnswer= document.createTextNode('Your e-mail is '+email+'!');
+    console.log(textAnswer);
+    /*h1.setAttribute('id','ageInDays'); //set the attribute id=AgeinDays() to the h1 element
+    h1.appendChild(textAnswer); //The appendChild() returns the appended child.
+    document.getElementById('flex-box-result').appendChild(h1);*/
+    }
+    
